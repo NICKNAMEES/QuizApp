6a5602450354fc720e8f2227b1af98b927d44b1e
NICKNAMEES <44614755+NICKNAMEES@users.noreply.github.com>
2020-09-14 08:42:06 -0400

    initial

---
+++ QuizApp.java
@@ -0,0 +1,15 @@
+
+package quizapp;
+
+/**
+ *
+ * @author Mohamed Abdrabbo
+ */
+public class QuizApp {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+    
+}
